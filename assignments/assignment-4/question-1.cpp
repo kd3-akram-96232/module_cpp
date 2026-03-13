320b4734be8210d169f4b83d083e3aee29f81696
Akram Khan <khanakram8435@gmail.com>
2026-03-13 17:15:09 +0530

    Refactor Time class: improve formatting, add increment methods, and update time initialization

--- assignments/assignment-4/question-1.cpp
+++ assignments/assignment-4/question-1.cpp
@@ -1,79 +1,142 @@
 #include <iostream>
 using namespace std;
 
-class Time {
+class Time
+{
     int hour;
     int minute;
     int second;
 
-    public: 
-    Time(): hour(0), minute(0), second(0){}
+public:
+    Time() : hour(0), minute(0), second(0) {}
 
-    Time(int hour, int minute, int second) {
-        if(hour >= 0 && hour <= 23){
+    Time(int hour, int minute, int second)
+    {
+        if (hour >= 0 && hour <= 23)
+        {
             this->hour;
         }
-        if(this->minute >= 0 && this->minute <= 59){
+        if (this->minute >= 0 && this->minute <= 59)
+        {
             this->minute;
         }
-        if(this->second >= 0 && this->second <= 59){
+        if (this->second >= 0 && this->second <= 59)
+        {
             this->second;
         }
     }
 
-    void setHour(int hour){
+    void setHour(int hour)
+    {
         this->hour = hour;
     }
-    int getHour(){
+    int getHour()
+    {
         return this->hour;
     }
-    void setMinute(int minute){
+    void setMinute(int minute)
+    {
         this->minute = minute;
     }
-    int getMinute(){
+    int getMinute()
+    {
         return this->minute;
     }
 
-    void setSecond(int second){
+    void setSecond(int second)
+    {
         this->second = second;
     }
-    int getSecond(){
+    int getSecond()
+    {
         return this->second;
     }
-    void addTime(int hour, int minute=0, int second=0){
+    void addTime(int hour, int minute = 0, int second = 0)
+    {
         this->hour = hour;
         this->minute = minute;
         this->second = second;
     }
 
-    void displayTime(){
-        cout << "Hour: " << this->hour << " Minute: " << this->minute << " Second: " << this->second<<endl;
+    // increment hour
+    void incrementHour()
+    {
+        if (this->hour < 23)
+        {
+            this->hour++;
+        }
+        else
+        {
+            this->hour = 0;
+        }
     }
 
+    // increment minute
+    void incrementMinute()
+    {
+        if (minute < 59)
+        {
+            this->minute++;
+        }
+        else
+        {
+            this->minute = 0;
+            incrementHour();
+        }
+    }
+
+    // increment second
+    void incrementSecond()
+    {
+        if (second < 59)
+        {
+            this->second++;
+        }
+        else
+        {
+            this->second = 0;
+            incrementMinute();
+        }
+    }
+
+    void displayTime()
+    {
+        cout << "Hour: " << this->hour << ", Minute: " << this->minute << ", Second: " << this->second << endl;
+    }
 };
 
-
-int main(){
+int main()
+{
 
     Time *times[5];
 
     // create dynamic objects
-    for(int i = 0; i<5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         times[i] = new Time();
     }
 
     // add time
-    for(int i = 0; i<5; i++){
-        times[i]->addTime(12, 12, i);
+    for (int i = 0; i < 5; i++)
+    {
+        times[i]->addTime(0, 0, 0);
     }
 
-    // read all object times 
-    for(int i=0; i<5; i++){
+    // testing
+    for (int i = 0; i < 86400; i++)
+    {
+        times[0]->incrementSecond();
+    }
+
+    // read all object times
+    for (int i = 0; i < 5; i++)
+    {
         times[i]->displayTime();
     }
 
     // display only hour of all objects
-    for(int i = 0; i<5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         int hour = times[i]->getHour();
         cout << "Hour of: " << i << " object" << hour << endl;
     }
